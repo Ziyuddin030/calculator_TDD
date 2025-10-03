37c442704d23344e11ddd63353eb7603a9cdcb36
Ziyuddin030 <mohammadziyauddin030@gmail.com>
2025-10-03 13:16:23 +0530

    Added tests for multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,7 +1,28 @@
-require "string_calculator"
+# frozen_string_literal: true
+
+require 'string_calculator'
 
 RSpec.describe StringCalculator do
-  it "returns 0 for an empty string" do
-    expect(StringCalculator.new.add("")).to eq(0)
+  let(:string_calculator) { StringCalculator.new }
+
+  describe '#add with basic inputs' do
+    it 'returns 0 for an empty string' do
+      expect(string_calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number itself if only one number is given' do
+      expect(string_calculator.add('1')).to eq(1)
+    end
+
+    it "sums two numbers" do
+      expect(string_calculator.add("1,5")).to eq(6)
+    end
+
+    context "with many numbers" do
+      it "sums more than two numbers" do
+        expect(string_calculator.add("1,2,3,4,5")).to eq(15)
+        expect(string_calculator.add("10,20,30,40,50,60")).to eq(210)
+      end
+    end
   end
 end

28d08a33a8e4d266c740c87714c0f887d5f82f90
Ziyuddin030 <mohammadziyauddin030@gmail.com>
2025-10-03 13:57:27 +0530

    Handled invalid input and negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,21 @@
 class StringCalculator
   def add(str)
     return 0 if str.empty?
-    numbers = str.split(/,|\n/).map(&:to_i)
+
+    delimiter = /,|\n/
+    if str.start_with?("//")
+      header, numbers_part = str.split("\n", 2)
+      custom_delimiter = header[2..]
+      delimiter = Regexp.new(Regexp.escape(custom_delimiter))
+      str = numbers_part
+    end
+
+    numbers = str.split(delimiter).map(&:to_i)
+    negatives = numbers.select(&:negative?)
+    if negatives.any?
+      raise "negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
     numbers.sum
   end
 end

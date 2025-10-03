9c0368d6114d2908d6d070821397c445458abae7
Ziyuddin030 <mohammadziyauddin030@gmail.com>
2025-10-03 14:01:54 +0530

    refactor: extract delimiter parsing and negative validation into private methods

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,20 +5,32 @@ class StringCalculator
   def add(str)
     return 0 if str.empty?
 
-    delimiter = /,|\n/
+    delimiter, numbers_str = extract_delimiter(str)
+    numbers = parse_numbers(numbers_str, delimiter)
+
+    validate_negatives!(numbers)
+
+    numbers.sum
+  end
+
+  private
+
+  def extract_delimiter(str)
     if str.start_with?("//")
       header, numbers_part = str.split("\n", 2)
       custom_delimiter = header[2..]
-      delimiter = Regexp.new(Regexp.escape(custom_delimiter))
-      str = numbers_part
+      [Regexp.new(Regexp.escape(custom_delimiter)), numbers_part]
+    else
+      [/[,|\n]/, str]
     end
+  end
 
-    numbers = str.split(delimiter).map(&:to_i)
+  def parse_numbers(str, delimiter)
+    str.split(delimiter).map(&:to_i)
+  end
+
+  def validate_negatives!(numbers)
     negatives = numbers.select(&:negative?)
-    if negatives.any?
-      raise "negative numbers not allowed: #{negatives.join(', ')}"
-    end
-
-    numbers.sum
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end

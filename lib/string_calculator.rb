1da7cccfe135ef2348987a94dd48a6ec2bb153d2
Ziyuddin030 <mohammadziyauddin030@gmail.com>
2025-10-03 13:18:02 +0530

    support multiple numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,8 @@
 # StringCalculator implements the classic TDD kata:
 class StringCalculator
   def add(str)
-    0
+    return 0 if str.empty?
+    numbers = str.split(",").map(&:to_i)
+    numbers.sum
   end
 end
